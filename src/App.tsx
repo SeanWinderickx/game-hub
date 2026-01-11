5172fdc02cae2e730bb2b2126af77e186b130df5
Sean <sean.winderickx@gmail.com>
2026-01-11 11:42:31 +0000

    Highlight selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,10 @@ function App() {
       </GridItem>
       <Show when={useBreakpointValue({ base: false, lg: true })}>
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">

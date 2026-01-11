e178538e0640b92608371a1fd6d56a5aa37f22f8
Sean <sean.winderickx@gmail.com>
2026-01-11 17:33:37 +0000

    Implement search bar

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -17,9 +16,11 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) => 
-  useData<Game>("/games", {params: 
-    {genres: gameQuery.genre?.id, 
-      platforms: gameQuery.platform?.id, 
-      ordering: gameQuery.sortOrder}}, [gameQuery]);
+  useData<Game>("/games", {params: {
+    genres: gameQuery.genre?.id, 
+    platforms: gameQuery.platform?.id, 
+    ordering: gameQuery.sortOrder,
+    search: gameQuery.searchText
+  }}, [gameQuery]);
 
 export default useGames;
